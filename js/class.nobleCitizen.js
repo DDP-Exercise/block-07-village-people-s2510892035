9ebee2a6e0dd235d4140e8b2127a920d21ff32b2
s2510892035 <s2510892035@students.fh-hagenberg.at>
2026-06-04 21:58:00 +0200

    Finished Village People assignment

--- js/class.nobleCitizen.js
+++ js/class.nobleCitizen.js
@@ -1,6 +1,19 @@
 "use strict";
+import Citizen from "./class.citizen.js";
+
 /**
  * Create a NobleCitizen class. A Noble Citizen is basically
  * the same as a regular citizen, just with rank 1 and another
  * representation in the Citizen Dictionary (see the img folder).
  */
+export default class NobleCitizen extends Citizen {
+    constructor(name) {
+        super(name);
+        this.rank = 1;
+    }
+
+    toString() {
+        return `${this.name}, Noble Citizen, Rank ${this.rank}`;
+    }
+
+}
--- js/class.village.js
+++ js/class.village.js
@@ -1,5 +1,10 @@
 "use strict";
 
+import Citizen from "./class.citizen.js";
+import NobleCitizen from "./class.nobleCitizen.js";
+import Building from "./class.building.js";
+import NobleBuilding from "./class.nobleBuilding.js";
+
 /**
  * Create a Village class. Each village should have
  * - a name
@@ -9,3 +14,52 @@
  * You can see in main.js what methods a village should provide.
  * implement them.
  */
+
+export default class Village{
+    constructor(name){
+        this.name = name;
+        this.buildings = [];
+        this.citizens = [];
+    }
+
+    addBuilding(name, capacity, noble = false){
+        this.buildings.push(noble ? new NobleBuilding(name, capacity) : new Building(name, capacity));
+    }
+
+    addCitizen(name, noble = false){
+        let citizen = noble ? new NobleCitizen(name) : new Citizen(name);
+        this.citizens.push(citizen);
+        this.shelterCitizen(citizen);
+    }
+
+    shelterCitizen(citizen){
+        for(const building of this.buildings){
+            if(building.addResident(citizen))
+                return;
+
+        }
+    }
+
+    shelterTheWorthy(){
+        for(const citizen of this.citizens){
+            if(citizen.home == null)
+                this.shelterCitizen(citizen);
+        }
+    }
+
+    printCitizenDirectory(){
+        //For all buildings: List your residents
+        for(const building of this.buildings) //für das brauchen wir die Buildings!!
+            building.listAllResidents();
+
+        this.listAllHomeless();
+    }
+
+    listAllHomeless(){
+        console.log("%c Homeless people of " + this.name + ":", "background-color: #a00; color:white");
+        for(const citizen of this.citizens){
+            if(citizen.home == null)
+                console.log(String(citizen));
+        }
+    }
+}
